a13d65705da3605fc2d7cb56898ce20fc8f301e1
Viczka <viktoriaandrusik@gmail.com>
2021-01-06 18:20:49 +0100

    8

---
+++ Lab2/zad8.py
@@ -0,0 +1,7 @@
+a = int(input("podaj długość boku a:"))
+b = int(input("podaj długość boku b:"))
+c = int(input("podaj długość boku c:"))
+if ((c * c) > (a * a) + (b * b)):
+    print("Trójkąt jest prostokątny")
+else:
+    print("trójkąt nie jest prostokątny")
