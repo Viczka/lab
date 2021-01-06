bf34c84f50caa747684a9cc7f5b652de573bb85a
Viczka <viktoriaandrusik@gmail.com>
2021-01-06 18:14:02 +0100

    6

---
+++ Lab1/zad6.py
@@ -0,0 +1,10 @@
+s1 = 30
+t1= 15
+s2 = 30
+t2 = 12
+v1 = (s1/(t1/60))
+v2 = (s2/(t2/60))
+print(("Prędkość 1 dystansu"), v1, ("km/h"))
+print(("Prędkość 2 dystansu"), v2, ("km/h"))
+vsr = (v1+v2)/2
+print(("Predkość srednia wynosi"), vsr,("km/h"))
