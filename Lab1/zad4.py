e7d585c33972436ce23bb87fa4e56f04345d9bf4
Viczka <viktoriaandrusik@gmail.com>
2021-01-06 18:12:11 +0100

    4

---
+++ Lab1/zad4.py
@@ -0,0 +1,7 @@
+s = 1
+m = 60 * s
+life = 21
+print(("An hour has"),m * 60, "seconds")
+print(("The day has"),m * 60 * 24, "seconds")
+print(("Year has"),m * 60 * 24 * 365, "seconds")
+print(("My life has"),m * 60 * 24 * 365 * life, "seconds")
