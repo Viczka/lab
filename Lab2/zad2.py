585f25ae3f885d2631421aaecb90fad84ff216c2
Viczka <viktoriaandrusik@gmail.com>
2021-01-06 18:17:08 +0100

    2

---
+++ Lab2/zad2.py
@@ -0,0 +1,6 @@
+x = int(input("Podaj liczbę x:"))
+if (x <= 10):
+    print("x jest mniejsza lub równa 10")
+elif (x > 10) and (x <= 25):
+    print("x jest większa od 10 i mniejsza lub równa 25")
+else: print("x jest wieksza od 25")
