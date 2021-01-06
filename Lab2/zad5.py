647d27c8ea5baf4b99cd5bca1d58b6600b5f1aca
Viczka <viktoriaandrusik@gmail.com>
2021-01-06 18:19:11 +0100

    5

---
+++ Lab2/zad5.py
@@ -0,0 +1,6 @@
+x = float(input("podaj wartość: "))
+if (x <= 0.2):
+    print("dopuszczalne steżenie alko..uważaj na drodze")
+elif(x > 0.2) and (x <= 0.5):
+    print("stan po użyciu alko..po co tyle piłeś/aś")
+else: print("jesteś nietrzeźwy")
