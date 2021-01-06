051d173eca9954a5c3473cd1306bd8066dfbb4d4
Viczka <viktoriaandrusik@gmail.com>
2021-01-06 18:33:00 +0100

    2

---
+++ Lab4/zad2.py
@@ -0,0 +1,12 @@
+#Dodatnie
+i = 254
+while i < 320:
+    i += 1
+    if (i % 2 == 1) and (i % 5 == 0):
+        print(i)
+#Ujemne
+i = -254
+while i > -320:
+    i -= 1
+    if (i % 2 == 1) and (i % 5 == 0):
+        print(i)
