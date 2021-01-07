d27fa149da48f97e116c4477467015fa74067844
Viczka <viktoriaandrusik@gmail.com>
2021-01-07 18:47:37 +0100

    5

---
+++ Lab5/zad5.py
@@ -0,0 +1,12 @@
+a = 0
+b = 1
+d = 0
+suma = 0
+list = []
+while b < 100:
+    c = (a + b) * ((a^2) - (a * b) + (b^2))
+    a += 1
+    b += 1
+    print("suma sześcianów z",a ,"i",b, "równa się",c)
+
+
---
+++ Lab5/zad6.py
@@ -0,0 +1,7 @@
+d = 0
+list = []
+if d == 0:
+    d += 1
+    list.append(d)
+    if len(list) == 15:
+        print(list)
