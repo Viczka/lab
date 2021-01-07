15649130bfe6ac1da2bdb50cf1c50b8a4630dc9c
Viczka <viktoriaandrusik@gmail.com>
2021-01-07 19:17:56 +0100

    5

--- Lab5/zad5.py
+++ Lab5/zad5.py
@@ -1,12 +1,17 @@
 a = 0
 b = 1
-d = 0
-suma = 0
 list = []
+d = 0
 while b < 100:
     c = (a + b) * ((a^2) - (a * b) + (b^2))
     a += 1
     b += 1
     print("suma sześcianów z",a ,"i",b, "równa się",c)
+while True:
+    d += 1
+    list.append(d)
+    if sum(list) >= 1000000:
+        print("żeby otrzymać liczbę większą od 1000000 potrzebujemy", len(list), "liczb naturalnych" )
+        break
 
 
