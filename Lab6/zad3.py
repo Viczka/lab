62eb0bcdf2f63b0129a127209d26870b47f49524
Viczka <viktoriaandrusik@gmail.com>
2021-01-08 14:08:11 +0100

    3

--- Lab6/zad3.py
+++ Lab6/zad3.py
@@ -13,7 +13,7 @@ print(tab)
 print("Teraz wykorzystamy metodę 'insert'")
 insert = 0
 while insert < 3:
-    index = int(input("Podaj index el. w liście:"))
+    index = int(input("Podaj index el. w tab:"))
     liczba = random.randrange(0, 60, 2)
     tab.insert(index, liczba)
     insert  += 1
@@ -39,7 +39,11 @@ print("Usuneliśmy elementy za pomocą 'remove':",tab)
 print("Teraz wykorzystamy metodę 'pop'")
 pop = 0
 while pop < 3:
-    index = int(input("Podaj index el. w liście:"))
+    index = int(input("Podaj index el. z tab:"))
     pop += 1
     tab.pop(index)
 print("Usuneliśmy elementy za pomocą 'pop':",tab)
+print("Teraz zamienimy el. 5 i 10 na 3 i 33")
+tab[5] = 3
+tab[10] = 33
+print(tab)
