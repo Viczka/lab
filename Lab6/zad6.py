ce0d27d9fa16a25a6c5d44f487de50df193b387b
Viczka <viktoriaandrusik@gmail.com>
2021-01-12 11:52:20 +0100

    6

--- Lab6/zad6.py
+++ Lab6/zad6.py
@@ -1,16 +1,16 @@
 import random
-tab6 = []
+a = []
 for i in range (0, 10):
-    tab6.append(random.randint(0, 7))
-print(tab6)
-tab6.sort()
-print(tab6)
+    a.append(random.randint(0, 15))
+print("Wygenerowany ciąg:",a)
+a.sort()
+print(a)
 y = 0
-while y<len(tab6):
-    z = tab6.count(tab6[y])
+while y < len(a):
+    z = a.count(a[y])
     if z>1:
-        print("Liczba", tab6[y] ,"powtarza się", z ,"raz/y")
+        print("Liczba", a[y] ,"powtarza się", z ,"raz/y")
     y +=z
-newTab6 = list(dict.fromkeys(tab6))
-print("Nowa tablica: ",newTab6)
-print("Długość nowej tablicy: ",len(newTab6))
+newa = list(dict.fromkeys(a))
+print("Nowa tablica: ",newa)
+print("Długość nowej tablicy: ",len(newa))
