039502ec578ed4e17ca9b3748d19ae5e988b67c1
Viczka <viktoriaandrusik@gmail.com>
2021-01-12 10:40:03 +0100

    5

--- Lab6/zad5.py
+++ Lab6/zad5.py
@@ -1,4 +1,4 @@
-# do poprawki ilość wystąpień el. w liście
+# do poprawki ilość wystąpień el. w liście (-39)
 import random
 a = []
 b = []
@@ -38,3 +38,9 @@ while i != len(c):
         print("Elementy, które występują 1 raz",d)
         print("Elementy, które występują kilka raz",set(t))
 
+
+""" 
+print("Liczba", c[i] ,"występuje 1 raz", z)
+else: print("Liczba", c[i], "powtarza się i występuje", z, "razy")
+d.append(c[i])
+i += 1  """
