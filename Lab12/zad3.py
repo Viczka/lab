7b100504f6bd63e7725b891b8ee1eed07c0393a2
Viczka <viktoriaandrusik@gmail.com>
2021-01-21 22:27:00 +0100

    3

--- Lab12/zad3.py
+++ Lab12/zad3.py
@@ -3,7 +3,7 @@ h = input("podaj długość w stopach:")
 def stop (h):
     cm = float(h) * 30.48
     mm = float(h) * 3048
-    km = float(h) * 0.345
+    km = float(h) * 0.0003048
     print("w cm:", cm, ',', "w mm:", mm, ",", "w km:", km)
     return cm, mm, km
 print(stop(h))
