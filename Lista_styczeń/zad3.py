a49b0d9b5fed67465ca7b11657064018100e5ddb
Viczka <viktoriaandrusik@gmail.com>
2021-01-29 22:54:29 +0100

    3

--- Lista_styczeń/zad3.py
+++ Lista_styczeń/zad3.py
@@ -1,100 +1,73 @@
 def test():
-    true = []
-    question1 = input("""1.Najważniejszym urządzeniem zestawu komputerowego jest: 
-    a) klawiatura
-    b) drukarka
-    c) jednostka centralna
-    Twoja odpowiedź?:""")
-    if question1 == 'c':
-        true.append(question1)
-    question2 = input("""2.Programy w systemie Windows uruchamiają się w postaci:
-    a) paska
+    correct = []
+    true = ['c','b','c','a','b','c','a','c','b','a','a']
+    questions = ['1.Najważniejszym urządzeniem zestawu komputerowego jest:',
+                 '2.Programy w systemie Windows uruchamiają się w postaci:',
+                 '3.Folder przeznaczony do szybkiego zapisywania plików w systemie Windows to:',
+                 '4.Czym jest zapisanie dokumentu:',
+                 '5.Twardy dysk to:',
+                 '6.Przycisk Start umożliwia:',
+                 '7.Komputer będzie działał wtedy, gdy będzie w nim zainstalowany:',
+                 '8.Pasek zadań znajduje się:',
+                 '9.Skrót klawiszowy przeznaczony do zapisywania to:',
+                 '10.Najważniejszą częścią elektroniczną , umieszczoną wewnątrz jednostki centalnej, nazywaną "mózgiem komputera" jest  :',
+                 '11.* Plik to:']
+    answers = ["""    a) klawiatura, 
+    b) drukarka, 
+    c) jednostka centralna""",
+    """    a) paska
     b) okien 
-    c) kartki
-    Twoja odpowiedź?:""")
-    if question2 == 'b':
-        true.append(question2)
-    question3 = input("""3.Folder przeznaczony do szybkiego zapisywania plików w systemie Windows to:
-    a) kosz
+    c) kartki""",
+    """    a) kosz
     b) mój komputer
-    c) moje dokumenty
-    Twoja odpowiedź?:""")
-    if question3 == 'c':
-        true.append(question3)
-    question4 = input("""4.Czym jest zapisanie dokumentu:
-    a) przekazanie informacji na dysk 
+    c) moje dokumenty""",
+    """    a) przekazanie informacji na dysk 
     b) przygotowanie dokumentu do druku
-    c) wyjście z programu
-    Twoja odpowiedź?:""")
-    if question4 == 'a':
-        true.append(question4)
-    question5 = input("""5.Twardy dysk to:
-    a) rodzaj drukarki
+    c) wyjście z programu""",
+    """    a) rodzaj drukarki
     b) pamięć komputera 
-    c) program komputerowy
-    Twoja odpowiedź?:""")
-    if question5 == 'b':
-        true.append(question5)
-    question6 = input("""6.Przycisk Start umożliwia:
-    a) drukowanie
+    c) program komputerowy""",
+    """    a) drukowanie
     b) zapisywanie dokumentów
-    c) uruchamianie programów zainstalowanych na komputerze
-    Twoja odpowiedź?:""")
-    if question6 == 'c':
-        true.append(question6)
-    question7 = input("""7.Komputer będzie działał wtedy, gdy będzie w nim zainstalowany:
-    a) system operacyjny 
+    c) uruchamianie programów zainstalowanych na komputerze""",
+    """    a) system operacyjny 
     b) edytor tekstu
-    c) program antywirusowy
-    Twoja odpowiedź?:""")
-    if question7 == 'a':
-        true.append(question7)
-    question8 = input("""8.Pasek zadań znajduje się:
-    a) na dyskietce
+    c) program antywirusowy""",
+    """    a) na dyskietce
     b) w folderze
-    c) w dolnej części Pulpitu
-    Twoja odpowiedź?:""")
-    if question8 == 'c':
-        true.append(question8)
-    question9 = input("""9.Skrót klawiszowy przeznaczony do zapisywania to :
-    a) Alt + Z
+    c) w dolnej części Pulpitu""",
+    """    a) Alt + Z
     b) Ctrl + S
-    c) Ctrl + Z
-    Twoja odpowiedź?:""")
-    if question9 == 'b':
-        true.append(question9)
-    question10 = input("""10.Najważniejszą częścią elektroniczną , umieszczoną wewnątrz jednostki centalnej, nazywaną "mózgiem komputera" jest  :
-    a) procesor
+    c) Ctrl + Z""",
+    """    a) procesor
     b) karta graficzna 
-    c) modem
-    Twoja odpowiedź?:""")
-    if question10 == 'a':
-        true.append(question10)
-    question11 = input("""11.* Plik to:
-    a) zapisana w komputerze informacja 
+    c) modem""",
+    """    a) zapisana w komputerze informacja 
     b) program komputerowy
-    c) urządzenie znajdujące się wewnątrz jednostki centralnej
-    Twoja odpowiedź?:""")
-    if question11 == 'a':
-        true.append(question11)
-    if len(true) == 0 or len(true) == 1:
-        return "Ocena 'negatywna' - 1"
-    if len(true) == 2 or len(true) == 3 :
-        return "Ocena 'dopuszczająca' - 2"
-    if len(true) == 4 or len(true) == 5:
-        return "Ocena 'dostateczna' - 3"
-    if len(true) ==6 or len(true) == 7:
-        return "Ocena 'dobra' - 4"
-    if len(true) >=8 and len(true) <= 10:
-        return "Ocena 'bardzo dobra' - 5"
-    if len(true) == 11:
+    c) urządzenie znajdujące się wewnątrz jednostki centralnej"""]
+    for i in range(0, len(true)):
+        print(questions[i])
+        print(answers[i])
+        x = input("Twoja odpowiedź?:")
+        if x == true[i]:
+            correct.append(x)
+    if len(correct) == 11:
         return "Ocena 'celująca' - 6"
+    elif len(correct) >=8 and len(correct) <= 10:
+        return "Ocena 'bardzo dobra' - 5"
+    elif len(correct) == 6 or len(correct) == 7:
+        return "Ocena 'dobra' - 4"
+    elif len(correct) == 4 or len(correct) == 5:
+        return "Ocena 'dostateczna' - 3"
+    elif len(correct) == 2 or len(correct) == 3:
+        return "Ocena 'dopuszczająca' - 2"
+    else:return "Ocena 'negatywna' - 1"
 
 def menu():
-    x = input("Chcesz rozwiązać test? Odpowiedz 'tak' lub 'nie':")
-    if x == 'nie':
+    x = input("Chcesz rozwiązać test? Odpowiedz 'tak' lub 'nie':").upper()
+    if x == 'NIE':
         print("Szkoda =(")
-    elif x == 'tak':
+    elif x == 'TAK':
         print('Nice =). To zaczynamy zabawe.'), print(test())
     else:print("Padałeś nie poprawną odpowiedź")
 menu()
