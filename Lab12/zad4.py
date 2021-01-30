bae4c344fea888d930ddd59d20cca90e84cb8a3e
Viczka <viktoriaandrusik@gmail.com>
2021-01-30 10:01:34 +0100

    4

--- Lab12/zad4.py
+++ Lab12/zad4.py
@@ -1,8 +1,10 @@
-# do poprawy - zrobić żeby zwracał dane typu float
 h = float(input("Na jakiej wysokości lecimy?[w metrach]:"))
 def wysokosc (h):
     a = float(h) / 1000
-    if a < 5:
-        return a
-    else:return "Na tej wysokości jesteś już bezpieczny"
-print(wysokosc(h),'km to za nisko!')
+    if a > 5:
+        print("Na tej wysokości jesteś już bezpieczny.")
+    else: print(a, 'km to za nisko!')
+wysokosc(h)
+
+
+
