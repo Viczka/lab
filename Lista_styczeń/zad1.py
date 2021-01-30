fc15bc71ce8acc1a81b4bba28fd43aeb4461531e
Viczka <viktoriaandrusik@gmail.com>
2021-01-30 10:30:12 +0100

    1

--- Lista_styczeń/zad1.py
+++ Lista_styczeń/zad1.py
@@ -1,22 +1,31 @@
-def netto(kwota_brutto):
-    emerytalna = 0.0976 * kwota_brutto
-    rentowa = 0.015 * kwota_brutto
-    chorobowa = 0.0245 * kwota_brutto
-    podstawa = kwota_brutto - (emerytalna + rentowa + chorobowa)
-    zdrowotna = podstawa * 0.09
-    p = emerytalna + rentowa + chorobowa + zdrowotna
-    kwota = kwota_brutto - p
-    zus_odliczenie = podstawa * 0.0775
-    zaliczka = kwota * 0.17 - 43.76 - zus_odliczenie
-    if zaliczka < 0:
-        zaliczka = 0
-
-    return round(kwota_brutto - (emerytalna + rentowa + chorobowa + zdrowotna + zaliczka), 2)
-
-
-if __name__ == "__main__":
-
-    kwota = float(input("Podaj kwote brutto: "))
-    print(f"Kwota netto: {netto(kwota)} zł")
-    zabiera = kwota - netto(kwota)
-    print(f"Dla Państwa:  {zabiera} zł ")
+import math
+def netto(x):
+    z = float(x) * 100 / 73.63
+    print("Ubezpieczenie emerytalne -","{:.3f}".format(float(z) * 9.76 / 100), "pln")
+    print("Ubezpieczenie rentowe -","{:.3f}".format(float(z) * 1.5 / 100), "pln")
+    print("Ubezpieczenie chorobowe -","{:.3f}".format(float(z) * 2.45 / 100), "pln")
+    print("Ubezpieczenie zdrowotne -","{:.3f}".format(float(z) * 7.77 / 100), "pln")
+    print("Zaliczka na PIT -","{:.3f}".format(float(z) * 4.89 / 100), "pln")
+    print("Kwota brutto -","{:.3f}".format(float(z)), "pln")
+def brutto(y):
+    print("Ubezpieczenie emerytalne -","{:.3f}".format(float(y) * 9.76 /100), "pln")
+    print("Ubezpieczenie rentowe -","{:.3f}".format(float(y) * 1.5 / 100), "pln")
+    print("Ubezpieczenie chorobowe -","{:.3f}".format(float(y) * 2.45 / 100), "pln")
+    print("Ubezpieczenie zdrowotne -","{:.3f}".format(float(y) * 7.77 / 100), "pln")
+    print("Zaliczka na PIT -","{:.3f}".format(float(y) * 4.89 / 100), "pln")
+    print("Kwota netto -","{:.3f}".format(float(y) * 73.63 / 100), "pln")
+def menu():
+    options = ['1 - netto',
+               '2 - brutto']
+    for i in range(0,len(options)):
+        print(options[i])
+    a = int(input("Podaj numer opcji, którą chcesz obliczyć:"))
+    if a == 1:
+        x = input("Podaj wynagrodzenie netto:")
+        print(netto(x))
+    if a == 2:
+        y = input("Podaj wynagrodzenie brutto:")
+        print(brutto(y))
+    elif a != 1 and a != 2:
+        print("Niema takiej opcji.")
+menu()
