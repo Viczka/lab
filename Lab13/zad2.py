07e63c1a98dae347927e6f1557e59b4475bcc609
Viczka <viktoriaandrusik@gmail.com>
2021-04-22 10:32:16 +0200

    3

--- Lab13/zad2.py
+++ Lab13/zad2.py
@@ -19,8 +19,8 @@ class book():
         return "Które z wydawnictw {} najbardziej Ci się podoba?".format(self.autor)
     def sprzedaj(self):
         return "Sprzedaję wszystkike ksiązki z rodzaju - '{}', ponieważ już zostały przeczytane przezemnie.".format(self.typ)
-    def kupuj(self, cena= input("Ile kosztuje książka?:")):
-       return "Teraz mozesz kupić książke {}. Po przecenie ona kosztuje tylko ".format(self.tytul) + cena + " pln.."
+    def kupuj(self, cena= input("Ile kosztuje książka po przecenie?:")):
+       return "W końcu kupię swoją ulubioną książke {}. Po przecenie ona kosztuje tylko ".format(self.tytul) + cena + " pln.."
     def wydanie(self):
         return "{} była wydana w {}.".format(self.tytul, self.rok)
     def sortuj(self):
@@ -32,8 +32,7 @@ list.append(book("„Harry Potter - seria", "J.K.Rowling", 1997, "literatura mł
 list.append(book("„Duma i uprzedzenie”", "Jane Austen", 1813 , "klasyka", "15.49 pln"))
 list.append(book("„Niewidzialne życie Addie LaRue”", "Victoria Schwab", 2020 , "fantastyka", "32.12 pln"))
 print(list[3].kupuj())
-print(list[0].czytaj(" bardzo ciekawa."))
+print(list[0].czytaj(" bardzo ciekawa"))
 print(list[1].wydanie())
 print(list[2].wybierz())
 print(list[4].sprzedaj())
-
