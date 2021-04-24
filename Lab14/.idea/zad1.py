9c6ffe96e887997925cad99df3bf2d419c0de576
Viczka <viktoriaandrusik@gmail.com>
2021-04-24 14:36:41 +0200

    1

---
+++ Lab14/.idea/zad1.py
@@ -0,0 +1,27 @@
+class pojazd():
+   def __init__ (self,marka, nr_tablicy):
+      self.marka = marka
+      self.nr_tablicy = nr_tablicy
+
+   def jedz(self):
+        print("Tu {}. Jedzie prosto i nawet skręca w prawo i w lewo.".format(self.marka))
+
+class samochod(pojazd):
+    def cofaj(self):
+        super().jedz()
+        print("A tak ogólnie to nawet cofa do tyłu.")
+
+class motocykl(pojazd):
+    def __init__(self,nr_tabicy,predkosc):
+        super().__init__("honda",nr_tabicy)
+        self.predkosc = predkosc
+
+    def stoj(self):
+        print("masakra co tu się dzieje, on ma numer",self.nr_tablicy,"i jedzie z prędkością",self.predkosc)
+
+honda = motocykl("YJ90O", 140)
+honda.stoj()
+honda.jedz()
+audi = samochod('audi', "GH678O")
+audi.cofaj()
+print(audi.marka)
