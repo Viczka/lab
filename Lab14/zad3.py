f8e645e7026f78df1084f0c94d906567a2938761
Viczka <viktoriaandrusik@gmail.com>
2021-04-28 11:33:49 +0200

    3

--- Lab14/zad3.py
+++ Lab14/zad3.py
@@ -1,105 +1,99 @@
 import math
 class figura():
-    def __init__(self,nazwa, bok_a):
+    def __init__(self,nazwa, a):
         self.nazwa = nazwa
-        self.bok_a = bok_a
+        self.a = a
 
     def przedstaw_sie(self):
         print("Siema, to jest {}. Obliczmy moje pole i obwód.".format(self.nazwa))
 
-
 class prostokat(figura):
-    def __init__(self,nazwa,bok_a,bok_b):
-        super().__init__(nazwa,bok_a)
-        self.bok_b = bok_b
+    def __init__(self,nazwa,a,b):
+        super().__init__(nazwa,a)
+        self.b = b
     def pole_i_obwod(self):
-        print("Pole prostokąta: ",(self.bok_a * self.bok_b))
-        print("Obwód prostokąta :", 2 * (self.bok_a + self.bok_b))
+        print("Pole prostokąta: ",(self.a * self.b))
+        print("Obwód prostokąta :", 2 * (self.a + self.b))
 
 class kwadrat(figura):
-    def __init__(self,nazwa,bok_a):
-        super().__init__(nazwa,bok_a)
+    def __init__(self,nazwa,a):
+        super().__init__(nazwa,a)
 
     def przedstaw_sie(self):
-        print(f"A ja jestem kwadratem i mam wszystkie strony takie same.Policz mnie, moja strona ma {self.bok_a} cm.")
+        print(f"Jestem kwadratem i mam wszystkie strony takie same.Policz mnie, moja strona ma {self.a} cm.")
 
     def pole_i_obwod(self):
-        print("Pole kwadrata: ",(self.bok_a * self.bok_a))
-        print("Obwód kwadrata :", 4 * self.bok_a)
+        print("Pole kwadrata: ",(self.a * self.a))
+        print("Obwód kwadrata :", 4 * self.a)
 
 class trojkat(prostokat):
-    def __init__(self,nazwa,bok_a,bok_b,wysokosc):
-        super().__init__(nazwa,bok_a,bok_b)
-        self.wysokosc = wysokosc
+    def __init__(self,nazwa,a,b,c,h):
+        super().__init__(nazwa,a,b)
+        self.c = c
+        self.h = h
 
     def pole_i_obwod(self):
-        print("Pole trójkąta: ",(1/2 * (self.bok_a*self.wysokosc)))
-        print("Odwód trójkągta:",self.bok_a+self.bok_b+self.wysokosc)
+        print("Pole trójkąta: ",(1/2 * (self.a*self.h)))
+        print("Odwód trójkągta:",self.a+self.b+self.c)
 
-class kolo(prostokat):
-    def __init__(self, nazwa, bok_a, bok_b):
-        super().__init__(nazwa, bok_a, bok_b)
+class kolo(figura):
+    def __init__(self, nazwa, a):
+        super().__init__(nazwa, a)
 
     def pole_i_obwod(self):
-            print("Pole koła: ", (self.bok_a * math.pi))
-            print("Odwód koła:", (math.pi * (self.bok_b + self.bok_b)))
+            print("Pole koła: ", (math.pi * (self.a ** 2) ))
+            print("Odwód koła:", (2 * math.pi * self.a))
 
-class romb(prostokat):
-    def __init__(self, nazwa, bok_a, bok_b):
-        super().__init__(nazwa, bok_a, bok_b)
+class romb(trojkat):
+    def __init__(self, nazwa, a, h):
+        super().__init__(nazwa, a,h)
 
     def przedstaw_sie(self):
         print(f"Z tej strony {self.nazwa}.")
 
     def pole_i_obwod(self):
-            print("Pole rombu: ", (self.bok_a * self.bok_b))
-            print("Odwód rombu:", (4 * self.bok_a))
+        print("Pole rombu: ", (self.a * self.h))
+        print("Odwód rombu:", (4 * self.a))
 
 class trapez(trojkat):
-    def __init__(self,nazwa,bok_a,bok_b,bok_c,wysokosc,):
-        super().__init__(nazwa,bok_a,bok_b,wysokosc)
-        self.bok_c = bok_c
+    def __init__(self, nazwa, a,b,c,d,h):
+        super().__init__(nazwa, a,b,c,h)
+        self.d=d
 
     def pole_i_obwod(self):
-        print("Pole trapezu: ",((self.bok_a+self.bok_b)*self.wysokosc)/2)
-        print("Odwód trapezu:",self.bok_a+self.bok_b+self.wysokosc+self.bok_c)
-def main():
-    f1 = prostokat("ptostokąt", 2,4)
-    f2 = kwadrat("kwadrat", 6)
-    f3 = trojkat("trójkąt", 2,5,3)
-    f4 = kolo("koło",4,2)
-    f5 = romb("romb",6,3)
-    f6 = trapez("trapez",3,6,1,2)
+        print("Pole trapezu: ", (1/2 * (self.a+self.b) * self.h))
+        print("Odwód trapezu:", self.a + self.b + self.c + self.d)
 
-    f1.przedstaw_sie()
-    f1.pole_i_obwod()
-    f2.przedstaw_sie()
-    f2.pole_i_obwod()
-    f3.pole_i_obwod()
-    f4.przedstaw_sie()
-    f4.pole_i_obwod()
-    f5.przedstaw_sie()
-    f5.pole_i_obwod()
-    f6.przedstaw_sie()
-    f6.pole_i_obwod()
+    f = {
+    1 : 'Prostokat',
+    2 : 'Kwadrat',
+    3 : 'Trójkat',
+    4 : 'Koło',
+    5 : 'Romb',
+    6 : 'Trapez'}
 
-if __name__ == "__main__":
-    main()
-
-#Siema, to jest ptostokąt. Obliczmy moje pole i obwód.
-#Pole prostokąta:  8
-#Obwód prostokąta : 12
-#A ja jestem kwadratem i mam wszystkie strony takie same.Policz mnie, moja strona ma 6 cm.
-#Pole kwadrata:  36
-#Obwód kwadrata : 24
-#Pole trójkąta:  3.0
-#Odwód trójkągta: 10
-#Siema, to jest koło. Obliczmy moje pole i obwód.
-#Pole koła:  12.566370614359172
-#Odwód koła: 12.566370614359172
-#Z tej strony romb.
-#Pole rombu:  18
-#Odwód rombu: 24
-#Siema, to jest trapez. Obliczmy moje pole i obwód.
-#Pole trapezu:  9.0
-#Odwód trapezu: 12
+    print(f)
+    k = float(input("Podaj numer figury, którą chcesz obliczyć : "))
+    if k == 0 or k > 6 or k < 0:
+        print("Niema takiego numeru.")
+    if k == 1:
+        f1 = prostokat("prostokąt",a=float(input("Podaj strone a : ")),b=float(input("Podaj strone b : ")))
+        f1.przedstaw_sie()
+        f1.pole_i_obwod()
+    if k == 2:
+        f2 = kwadrat("kwadrat", a=float(input("Podaj strone a : ")))
+        f2.przedstaw_sie()
+        f2.pole_i_obwod()
+    if k == 3:
+        f3 = trojkat("trójkąt",a=float(input("Podaj strone a : ")),b=float(input("Podaj strone b : ")),c=float(input("Podaj strone c : ")),h=float(input("Podaj wysokość : ")))
+        f3.pole_i_obwod()
+    if k == 4:
+        f4 = kolo("koło", a=float(input("Podaj promień : ")))
+        f4.pole_i_obwod()
+    if k == 5:
+        f5 = romb("romb",a=float(input("Podaj strone a : ")),h=float(input("Podaj wysokość : ")))
+        f5.przedstaw_sie()
+        f5.pole_i_obwod()
+    if k == 6:
+        f6 = trapez("trapez",a=float(input("Podaj strone a : ")),b=float(input("Podaj strone b : ")),c=float(input("Podaj strone c : ")),d=float(input("Podaj strone d : ")),h=float(input("Podaj wysokość : ")))
+        f6.pole_i_obwod()
